b47c4fa53edd81ca03b406d73fd66dabc64118e2
Tapan Sharma <tapansharma69@gmail.com>
2024-01-27 22:47:54 +0545

    define form object for update form

--- app/Livewire/EditStudent.php
+++ app/Livewire/EditStudent.php
@@ -3,59 +3,39 @@
 namespace App\Livewire;
 
 use App\Models\Classes;
-use App\Models\Section;
 use App\Models\Student;
 use Livewire\Component;
 use Livewire\Attributes\Validate;
+use App\Livewire\Forms\UpdateStudentForm;
 
 class EditStudent extends Component
 {
     public Student $student;
 
-    #[Validate('required')]
-    public $name;
-
-    public $email;
+    public UpdateStudentForm $form;
 
     #[Validate('required')]
     public $class_id;
 
-    #[Validate('required')]
-    public $section_id;
-
-    public $sections = [];
-
     public function mount()
     {
-        $this->fill($this->student->only([
-            'name',
-            'email',
-            'class_id',
-            'section_id',
-        ]));
+        $this->form->setStudent($this->student);
 
-        $this->sections = Section::where('class_id', $this->class_id)->get();
+        $this->fill($this->student->only([
+            'class_id',
+        ]));
     }
 
     public function updateStudent()
     {
-        $this->validate([
-            'email' => 'required|email|unique:students,email,' . $this->student->id,
-        ]);
-
-        $this->student->update([
-            'name' => $this->name,
-            'email' => $this->email,
-            'class_id' => $this->class_id,
-            'section_id' => $this->section_id,
-        ]);
+        $this->form->updateStudent($this->class_id);
 
         return redirect(route('students.index'));
     }
 
     public function updatedClassId($value)
     {
-        $this->sections = Section::where('class_id', $value)->get();
+        $this->form->setSections($value);
     }
 
     public function render()
---
+++ app/Livewire/Forms/UpdateStudentForm.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Livewire\Forms;
+
+use Livewire\Form;
+use App\Models\Section;
+use App\Models\Student;
+use Livewire\Attributes\Validate;
+
+class UpdateStudentForm extends Form
+{
+    public ?Student $student;
+
+    #[Validate('required')]
+    public $name;
+
+    #[Validate('required|email|unique:students,email')]
+    public $email;
+
+    #[Validate('required')]
+    public $section_id;
+
+    public $sections = [];
+
+    public function setStudent(Student $student)
+    {
+        $this->student = $student;
+
+        $this->fill($student->only([
+            'name',
+            'email',
+            'section_id',
+        ]));
+
+        $this->sections = Section::where('class_id', $student->class_id)->get();
+    }
+
+    public function updateStudent($class_id)
+    {
+        $this->validate([
+            'email' => 'required|email|unique:students,email,' . $this->student->id,
+        ]);
+
+        $this->student->update([
+            'name' => $this->name,
+            'email' => $this->email,
+            'class_id' => $class_id,
+            'section_id' => $this->section_id,
+        ]);
+    }
+
+    public function setSections($class_id)
+    {
+        $this->sections = Section::where('class_id', $class_id)->get();
+    }
+}
